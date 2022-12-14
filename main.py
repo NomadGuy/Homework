fbc669b766f35b86a77a9be600bd23c715867acc
Valeriy <valerijbilenko3@gmail.com>
2022-12-14 13:02:42 +0200

    Function encapsulation.

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-from re import sub
+import re
 
 
-def to_snake_case(replacer):
-    return '_'.join(
-        sub('([A-Z][a-z]+)', r' \1',
-            sub('([A-Z]+)', r' \1',
-                replacer.replace('-', ' '))).split()).lower()
+class NameConverter:
+    def __init__(self, convert: str):
+        self.convert = convert;
+
+    def to_snake_case(self):
+        return '_'.join(
+            re.sub('([A-Z][a-z]+)', r' \1',
+                   re.sub('([A-Z]+)', r' \1',
+                          self.convert.replace('-', ' '))).split()).lower()
+
+    def to_camel_case(self):
+        capitalize_first = input('Capitalize first? -  ')
+        s = re.sub(r"(_|-)+", " ", self.convert).title().replace(" ", "")
+        if capitalize_first == 'False' or capitalize_first == 'false':
+            return ''.join([s[0].lower(), s[1:]])
+        if capitalize_first == 'True' or capitalize_first == 'true':
+            return ''.join([s[0].upper(), s[1:]])
 
 
-def to_camel_case(replacer):
-    capitalize_first = input('Capitalize first? -  ')
-    s = sub(r"(_|-)+", " ", replacer).title().replace(" ", "")
-    if capitalize_first == 'False' or capitalize_first == 'false':
-        return ''.join([s[0].lower(), s[1:]])
-    if capitalize_first == 'True' or capitalize_first == 'true':
-        return ''.join([s[0].upper(), s[1:]])
-
-
-print(to_snake_case('IdIoTic-CaseCase'))
+if __name__ == '__main__':
+    a = NameConverter('MariaIvanovna')
+    b = NameConverter('maria_ivanovna')
+    print(a.to_snake_case())
+    print(b.to_camel_case())
